8e4116ca1d56e4270dbe1b5371ae3bef8b29d370
chintanshah46 <58828374+chintanshah46@users.noreply.github.com>
2023-03-09 22:37:48 -0500

    Reverting back to previous version

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,93 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useState } from 'react';
+import Input from './components/Input';
+import Highlighter from './components/Highlighter';
+import './App.css';
+
 function App() {
+  const [search, setSearch] = useState('');
+  const [css, setCss] = useState('');
+  const areaRef = useRef<HTMLInputElement>(null);
+
+  const onFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    areaRef.current = e.currentTarget;
+  };
+
+  const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (areaRef.current && areaRef.current.contains(e.target)) {
+      e.target.value = '';
+      setSearch((search) => e.target.value);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch((search) => e.target.value);
+
+    var hl_css = e.currentTarget.getAttribute('highlight_css');
+    setCss((css) => hl_css);
+  };
+
+  const onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const re = /[0-9a-zA-Z]+/g;
+    if (!re.test(e.key)) {
+      e.preventDefault();
+    }
+  };
+
   return (
-    <div className="">
-      <img className="" alt="Display" />
-      <div className="">
-        <div className="">Blessing Krofegha</div>
-        <p className="">
-          When i’m not coding i switch to netflix with biscuits and cold tea as
-          my companion. <span></span>😜
-        </p>
-      </div>
-      <div className="">
-        <span className="">#Software Engineer</span>
-        <span className="">#Writter</span>
-        <span className="">#Public Speaker</span>
+    <div className="App">
+      <Input
+        name="underline"
+        label="Under Line Style"
+        type="text"
+        placeholder="Search for underlined"
+        className="search"
+        highlight_css="underline"
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onKeyPress={onKeyPress}
+      ></Input>
+
+      <Input
+        name="bold"
+        label="Bold Style"
+        type="text"
+        placeholder="Search for bold effect"
+        className="search"
+        highlight_css="bold"
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onKeyPress={onKeyPress}
+      ></Input>
+
+      <Input
+        name="red"
+        label="Red Font Style"
+        type="text"
+        placeholder="Search for Red font effect"
+        className="search"
+        highlight_css="red"
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onKeyPress={onKeyPress}
+      ></Input>
+
+      <div className="text">
+        <Highlighter highlight={search} css={css}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Highlighter>
       </div>
     </div>
   );
 }
+
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './assets/main.css';
 import App from './App';
 import './index.css';
-import './assets/main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// ReactDOM.render(<App />, document.getElementById('root'));
