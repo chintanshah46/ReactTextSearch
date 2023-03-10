4bc5962902a353815db32e30c050495c31cb173b
chintanshah46 <58828374+chintanshah46@users.noreply.github.com>
2023-03-09 22:32:52 -0500

    tailwind broke the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/main.css';
-import App from './App';
-// import './index.css';
 // import './assets/main.css';
+import App from './App';
+import './index.css';
+import './assets/main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// ReactDOM.render(<App />, document.getElementById('root'));
